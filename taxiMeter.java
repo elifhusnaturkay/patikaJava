10301aa77e6a5f34d6f5de676bd495b69f92b79c
Elif Hüsna Turkay <elifhusnaturkay@hotmail.com>
2023-11-18 05:00:13 +0300

    Taxi fare calculator program for distance traveled

---
+++ taxiMeter.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class taxiMeter {
+   public static void main(String[]args){
+      Scanner keyboard = new Scanner(System.in);
+      double gidilenKM, toplamKmDegeri, tutar, taksiAcilisUcreti;
+
+      taksiAcilisUcreti = 10.0;
+      System.out.print("Gidilen KM\'yi giriniz: ");
+      gidilenKM = keyboard.nextDouble();
+      toplamKmDegeri = (gidilenKM * 2.20) +taksiAcilisUcreti;
+
+      if(toplamKmDegeri <= 20){
+         tutar = 20;
+      }else{
+         tutar = toplamKmDegeri;
+      }
+
+      System.out.println("Ödemeniz gereken tutar: " + tutar);
+   }
+}
