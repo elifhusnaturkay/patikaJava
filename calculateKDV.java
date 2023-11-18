d1d6c38d3df3bb1db5ec428fd6ee219afcb1feea
Elif Hüsna Turkay <elifhusnaturkay@hotmail.com>
2023-11-18 03:33:36 +0300

    A program that calculates KDV based on the user-entered amount

---
+++ calculateKDV.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class calculateKDV {
+   public static void main(String[] args) {
+   Scanner keyboard = new Scanner(System.in);
+
+   System.out.println("Ürün tutarını giriniz:");
+   double urunFiyat = keyboard.nextInt();
+
+   double urunKDVOrani;
+      if(urunFiyat>=1000){
+      urunKDVOrani = urunFiyat * 0.08 ;
+      }else{
+      urunKDVOrani = urunFiyat * 0.18;
+      }
+   
+   System.out.println("Ürünün KDV ücreti: " + urunKDVOrani);
+
+   double urunKDVFiyat = urunFiyat + urunKDVOrani;
+   System.out.println("Ürünün KDV dahil fiyatı: " + urunKDVFiyat);
+   }
+}
